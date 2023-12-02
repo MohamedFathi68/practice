3e8195e679695f2718b05689fe26e62d8984f74e
Mohamed Fathi <mohamedfathielqastawy@gmail.com>
2023-12-02 11:06:06 +0200

    data types

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
 document.getElementById('a').innerHTML="hello"
 
+// datatypes
+
+// primitive
+
+// String
+var userName ;
+userName = "mohamed" ;
+console.log(typeof userName + " " + userName);
+
+// Number
 var userAge ;
 userAge = 21 ;
-userAge = 22 ;
 console.log(typeof userAge + " " + userAge);
+
+// Boolean
+var isOld ;
+isOld = true ;
+console.log(typeof isOld + " " + isOld);
+
+// undefined
+var gender ;
+console.log(gender)
+
+// null
+console.log(typeof(null))
+
+
+// none-primitive
+
+// Object
